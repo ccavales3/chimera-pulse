a839d1729abc69d8ef5ddbefa0d9d4946e3a89bf
Nermina Albukhary <snalbukhary@email.meredith.edu>
2024-01-02 11:46:07 -0500

    PR changes

--- chimerapulse/core/language/sentiment_analysis.py
+++ chimerapulse/core/language/sentiment_analysis.py
@@ -2,10 +2,12 @@
 https://learn.microsoft.com/en-us/azure/ai-services/language-service/sentiment-opinion-mining/overview?tabs=prebuilt
 
 Example:
-    python -m chimerapulse.core.language analyzesentiment -t 'The food was great'
+    python -m chimerapulse.core.language analyzesentiment -d 'The food was great'
+    chi language analyzesentiment <args>
 """
 
 import os
+from tkinter import filedialog
 
 import click
 from dotenv import load_dotenv
@@ -15,11 +17,6 @@ from azure.core.credentials import AzureKeyCredential
 
 # Load env vars
 load_dotenv()
-
-@click.command()
-@click.option('-t', 'text', required=True, help='Text to analyze')
-def analyzesentiment(text):
-    language_analyzesentiment(text)
     
 def __authenticate_client():
     """Authenticate service
@@ -36,6 +33,46 @@ def __authenticate_client():
             endpoint=languageendpoint, 
             credential=credential)
     return text_analytics_client
+
+"""
+Validation fxns
+"""
+def get_document_file_path(document, document_file_path):
+    """Retrieves document. If document is provided, it takes precedence than retrieving from document file path.
+
+    Args:
+        document (str): Document context
+        document_file_path (str): Path to document file
+
+    Return:
+        (str): Document
+    """
+    if not document and not document_file_path:
+        raise ValueError('Neither -d/--document or -p/--document-file-path values were provided. Exiting...')
+
+    # Takes precedence than retrieving document from file path
+    if document:
+        return document
+
+    if document_file_path == 'flag':
+        filepath = filedialog.askopenfilename()
+    else:
+        filepath = os.path.relpath(document_file_path, os.getcwd())
+
+    # Validate file path if value is provided
+    if not os.path.isfile(filepath):
+        raise ValueError(f'ERROR: Cannot find document file at: {document_file_path}. Exiting...')
+
+    file_contents = open(filepath, 'r')
+
+    return file_contents.read()
+
+@click.command()
+@click.option('-d', '--document', help='Text to analyze')
+@click.option('-p', '--document-file-path', flag_value='flag', is_flag=False, default=None, help='Path to document file')
+def analyzesentiment(document, document_file_path):
+    document_contents = get_document_file_path(document, document_file_path)
+    language_analyzesentiment(document_contents)
 
 def language_analyzesentiment(text):
     client = __authenticate_client()
