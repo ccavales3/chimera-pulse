6f93aa56f09d35d54e187e1222efe21beda0fb37
Caesar Cavales <caesarcavales@Caesars-MBP.attlocal.net>
2024-01-03 09:34:02 -0500

    added new fxns to commands list

--- chimerapulse/commands.py
+++ chimerapulse/commands.py
@@ -25,9 +25,9 @@ def commands():
 
 # TODO: Change "main" to more appropriate name
 # ChimeraPulse main commands
-commands.add_command(chimerapulse.case1)
-commands.add_command(chimerapulse.case2)
-commands.add_command(chimerapulse.case3)
+commands.add_command(chimerapulse.translatespeech)
+commands.add_command(chimerapulse.convosummarization)
+commands.add_command(chimerapulse.docsummarization)
 
 # ChimeraPulse core capabilities
 commands.add_command(language.language)
